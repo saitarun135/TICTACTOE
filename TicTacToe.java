f748bdd4bd5192b70ea73166476fe8521f05b370
saitarun135 <saitarun135@gm.com>
2021-03-16 09:12:58 +0530

    create board for game

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,1 +1,12 @@
-#Wlecome to the Tic-Tac-Toe game.....!
+public class TicTacToe{
+	static void createBoard() {
+		char[] board=new char[10];
+		for(int i=1;i<board.length;i++) {
+			board[i]=' ';
+		}
+	}
+	public static void main(String args[]) {
+		System.out.println("Welcome to the TIC_TAC_TOE game");
+		createBoard();
+	}
+}
