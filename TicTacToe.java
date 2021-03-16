d966ba05e9879ba91567bb8af17fef7a3e1cd5ba
saitarun135 <saitarun135@gm.com>
2021-03-16 14:27:00 +0530

    Toss feature added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,77 +1,71 @@
 import java.util.*;
 public class TicTacToe {
-    static char[] board = new char[10];
-    public static char player;
-    public static char computer;
-    public static void makeEmpty() {
-        for (int i = 1; i < board.length; i++) {
-            board[i] = ' ';
+    public static int index;
+    public static char[] board;
+    public static Scanner sc = new Scanner(System.in);
+
+
+    public static char[] createBorad() {
+        char[] board = new char[10];
+        for (index = 1; index < board.length; index++) {
+            board[index] = ' ';
+        }
+        return board;
+    }
+
+ 
+    public static char userInput() {
+        System.out.println("Enter the input X/O.");
+        char userInput = sc.next().toUpperCase().charAt(0);
+        int computer = (userInput == 'X') ? 'O' : 'X';
+        return userInput;
+    }
+
+  
+    public static char[] displayBoard() {
+            index = 1;
+            System.out.println(" | " + board[index] + " | " + board[index + 1] + " | " + board[index + 2] + " | ");
+            System.out.println(" | " + board[index + 3] + " | " + board[index + 4] + " | " + board[index + 5] + " | ");
+            System.out.println(" | " + board[index + 6] + " | " + board[index + 7] + " | " + board[index + 8] + " | ");
+        return board;
+    }
+
+   
+    private static int makeMove(char [] board) {
+        Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        while (true) {
+            System.out.println("What is the next move? (1-9): ");
+            int userIndex = sc.nextInt();
+            if (Arrays.asList(validCells).contains(userIndex) && isSpaceFree(board, userIndex)){
+                board[userIndex] = userInput();
+                displayBoard();
+                takeToss();
+            }
+                return userIndex;
         }
     }
 
-    public static void playerSelect() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the Symbol you choose to with (X/O): ");
-        String choice = scan.next();
-        switch(choice) {
-            case "X":
-                player = 'X';
-                computer = 'O';
-                System.out.println("The player Entered option is" +player);
-                break;
-            case "O":
-                player = 'O';
-                computer = 'X';
-                System.out.println("The player Entered option is" +player);
-                break;
-            default:
-                System.out.println("Invalid choice");
-                break;
-        }
-    }
 
-
-    public static void showBoard() {
-        System.out.println("Currently the Board Looks Like Following ");
-        System.out.println(" ");
-        System.out.println("\n "+ board[1] + " | "+ board[2] + " | " + board[3]);
-        System.out.println(" "+ board[4] + " | " + board[5] + " | " + board[6]);
-        System.out.println(" "+ board[7] + " | " + board[8] + " | " + board[9]);
-        System.out.println(" ");
-       // playerTurn();
-    }
-
-    public static void playerTurn() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the position where you want to make your move (1-9): ");
-
-        Integer[] valid = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int pos = scan.nextInt();
-        if (Arrays.asList(valid).contains(pos) && checkEmpty(pos)) {
-            board[pos] = player;
-            showBoard();
-        }else {
-            System.out.println("Invalid Choice, please enter a number between 1-9.");
-            playerTurn();
+    private static boolean isSpaceFree(char[] board, int index){
+            return board[index] == ' ';
         }
 
+    private static boolean takeToss() {
+        System.out.println("Enter the number\n1 for Head\n2 for Tail");
+        int choice = sc.nextInt();
+        int random = (int) Math.floor(Math.random() * 2 );
+        boolean won = (random == choice) ? true : false;
+        return won;
     }
 
-    public static boolean checkEmpty(int pos) {
-        if(board[pos] == ' ') {
-            return true;
-        }else {
-            System.out.println("The Position you entered is already filled. Please select the position that is empty.");
-            playerTurn();
-        }
-        return false;
-    }
-
+   
     public static void main(String[] args) {
-    	System.out.println("------------welcome to the TIC_TAC_TOE Game---------------------");
-        makeEmpty();
-        playerSelect();
-        showBoard();
-        playerTurn();
+        System.out.println("Welcome to Tic Tac Toe Game.");
+        board = createBorad();
+        displayBoard();
+        boolean wonOrLoss = takeToss();
+        int userMove = makeMove(board);
+
+
     }
 }
