0232d42866505228974696e283e1b269763162d1
saitarun135 <saitarun135@gm.com>
2021-04-06 13:29:39 +0530

    [sai]Added:refactoring code

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,245 +1,240 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TicTacToe {
+	private static Scanner in = new Scanner(System.in);
+	private static Board board = new Board();
 
-	private static final int TAILS = 0;
-	private static final int HEADS = 1;
-	private static final String PLAYER = "PLAYER";
-	private static final String COMPUTER = "COMPUTER";
-	static Scanner sc = new Scanner(System.in);
+	private static boolean gameEnded = false;
+	private static boolean player = playerturn();
+	private static boolean sessionEnded = false;
 
-	/**
-	 * uc1
-	 * 
-	 * @return
-	 **/
-
-	public static char[] boardCreation() {
-		char[] board = new char[10];
-		for (int i = 0; i < 10; i++) {
-			board[i] = ' ';
-		}
-		return board;
-	}
-
-
-	/**
-	 *  uc2
-   *
-	 * @param letter
-	 **/
-
-	public static char chooseLetter(char letter) {
-		System.out.println("Enter letter X or O for player");
-		 letter = sc.next().charAt(0);
-		return letter;
-	}
-
-	/**
-	 * uc3
-	 * 
-	 * @param board
-	 */
-
-	public static void displayBoard(char[] board) {
-		System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
-		System.out.println("-----------");
-		System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + " ");
-		System.out.println("-----------");
-		System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + " ");
-	}
-
-	/**
-	 * uc4
-	 * 
-	 * @param position
-	 * @param board
-	 * @param input
-	 */
-	public static void choosePosition(int position, char[] board, char input) {
-		while (true) {
-			if (freeSpace(board, position)) {
-				System.out.println("Position is free");
-				board[position] = input;
-				displayBoard(board);
-				break;
-
-			} else
-			{
-				System.out.println("Position isnt free, enter another position");
-				displayBoard(board);
-				position = sc.nextInt();
-				choosePosition(position,board,input);
-				break;
+	public static void main(String[] args) {
+		System.out.println(board);
+		while (!sessionEnded) {
+			while (!gameEnded) {
+				Position position = null;
+				if (player) {
+					position = makeMove();
+					board = new Board(board, position, PlayerSign.Cross);
+				} else {
+					board = computerMove(board);
+				}
+				player = !player;
+				System.out.println(board);
+				evaluateGame();
 			}
-			
+			System.out.println("Do you want to play another game? Y or N:");
+			String answer = in.nextLine();
+			if (answer.equalsIgnoreCase("N")) {
+				System.out.println("Exiting the game");
+				sessionEnded = true;
+			} else {
+				board = new Board();
+				System.out.println("Starting a new game");
+				gameEnded = false;
+				player = playerturn();
+			}
+
 		}
 	}
 
-	/**
-	 * uc4
-	 * 
-	 * @param board
-	 * @param position
-	 * @return
-	 */
-	public static boolean freeSpace(char[] board, int position) {
-		if (board[position] == ' ')
+	private static boolean playerturn() {
+		int turn = (int) (Math.random() * 10 % 2);
+		if (turn == 0) {
+			System.out.println("Player first");
+			return true;
+		} else {
+			System.out.println("Computer first");
+			return false;
+		}
+
+	}
+
+	private static Board computerMove(Board board) {
+		ArrayList<Position> positions = board.getFreePositions();
+		Board bestChild = null;
+		for (Position p : positions) {
+			if ((p.getRow() == 0 && p.getColumn() == 0) || p.getRow() == 2 && p.getColumn() == 0
+					|| p.getRow() == 2 && p.getColumn() == 2 || p.getRow() == 0 && p.getColumn() == 2) {
+				return new Board(board, p, PlayerSign.Circle);
+			}
+		}
+		for (Position p : positions) {
+			if ((p.getRow() == 1 && p.getColumn() == 1)) {
+				return bestChild = new Board(board, p, PlayerSign.Circle);
+			}
+		}
+		for (Position p : positions) {
+			return bestChild = new Board(board, p, PlayerSign.Circle);
+		}
+
+		return bestChild;
+
+	}
+
+	private static void evaluateGame() {
+		GameState gameState = board.getGameState();
+		gameEnded = true;
+		switch (gameState) {
+			case CrossWin:
+				System.out.println("You Won!");
+				break;
+			case CircleWin:
+				System.out.println("Computer Won!");
+				break;
+			case Draw:
+				System.out.println("Draw!");
+				break;
+			default:
+				gameEnded = false;
+				break;
+		}
+	}
+
+	public static Position makeMove() {
+		Position position = null;
+		while (true) {
+			System.out.print("Pick 1,2 or 3 for row: ");
+			int row = getColOrRow();
+			System.out.print("Pick 1,2 or 3 for column: ");
+			int column = getColOrRow();
+			position = new Position(column, row);
+			if (board.isMarked(position))
+				System.out.println("Already marked!");
+			else
+				break;
+		}
+		return position;
+	}
+
+	private static int getColOrRow() {
+		int ret = -1;
+		while (true) {
+			try {
+				ret = Integer.parseInt(in.nextLine());
+			} catch (NumberFormatException e) {
+			}
+			if (ret < 1 | ret > 3)
+				System.out.print("\nInvalid input. Please pick 1,2 or 3: ");
+			else
+				break;
+		}
+		return ret - 1;
+	}
+}
+
+final class Position {
+	private final int column;
+	private final int row;
+
+	public Position(int column, int row) {
+		this.column = column;
+		this.row = row;
+	}
+
+	public int getRow() {
+		return this.row;
+	}
+
+	public int getColumn() {
+		return this.column;
+	}
+}
+
+enum PlayerSign {
+	Cross, Circle
+}
+
+enum GameState {
+	Incomplete, CrossWin, CircleWin, Draw
+}
+
+class Board {
+	private char[][] board; // e = empty, x = cross, o = circle.
+
+	public Board() {
+		board = new char[3][3];
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++)
+				board[x][y] = 'e'; // Board initially empty
+	}
+
+	public Board(Board from, Position position, PlayerSign sign) {
+		board = new char[3][3];
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++)
+				board[x][y] = from.board[x][y];
+		board[position.getColumn()][position.getRow()] = sign == PlayerSign.Cross ? 'x' : 'o';
+	}
+
+	public ArrayList<Position> getFreePositions() {
+		ArrayList<Position> retArr = new ArrayList<Position>();
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++)
+				if (board[x][y] == 'e')
+					retArr.add(new Position(x, y));
+		return retArr;
+	}
+
+	public GameState getGameState() {
+		if (hasWon('x'))
+			return GameState.CrossWin;
+		else if (hasWon('o'))
+			return GameState.CircleWin;
+		else if (getFreePositions().size() == 0)
+			return GameState.Draw;
+		else
+			return GameState.Incomplete;
+	}
+
+	private boolean hasWon(char sign) {
+		int x, y;
+
+		// Check diagonals
+		if (board[0][0] == sign && board[1][1] == sign && board[2][2] == sign)
+			return true;
+		if (board[0][2] == sign && board[1][1] == sign && board[2][0] == sign)
 			return true;
 
-		else
-			return false;
-
-	}
-
-	/**
-	 * uc6
-	 * 
-	 * @return
-	 */
-	public static String toss() {
-		int toss = (int) Math.floor(Math.random() * 10 % 2);
-		if (toss == HEADS)
-			return PLAYER;
-		else
-			return COMPUTER;
-
-	}
-
-
-	/**
-	 * uc7
-	 * 
-	 * @param board
-	 * @param input
-	 * @return
-	 */
-
-	public static String result(char[] board, char input) {
-		int flag = 0;
-		for (int i = 0; i < 10; i++) {
-			if (board[i] == ' ') {
-				flag = 1;
-				break;
-			}
-		}
-
-		if ((board[0] == input && board[1] == input && board[2] == input)
-				|| (board[3] == input && board[4] == input && board[5] == input)
-				|| (board[6] == input && board[7] == input && board[8] == input)
-				|| (board[0] == input && board[3] == input && board[6] == input)
-				|| (board[1] == input && board[4] == input && board[7] == input)
-				|| (board[2] == input && board[5] == input && board[8] == input)
-				|| (board[0] == input && board[4] == input && board[8] == input)
-				|| (board[2] == input && board[1] == input && board[6] == input))
-
-			return "WIN";
-		else if (flag == 1)
-			return "CHANGE TURN";
-		else
-			return "TIE";
-
-	}
-  
-	
-	public static char swapInput(char input,char playerLetter,char computerLetter) {
-		if(input == playerLetter)
-			input = computerLetter;
-		else
-			input = playerLetter;
-		return input;
-	}
-
-	public static String swapTurn(String gamer) {
-		if(gamer==PLAYER)
-			gamer = COMPUTER;
-		else
-			gamer = PLAYER;
-		return gamer;
-	}
-	
-	public static int computerWin(char[] board, char input)
-	{
-		String computerWinPossibility;
-		int positionForComputerWin = 10;
-		
-		for(int i = 0; i <10; i++) {
-			if(board[i] == ' ') {
-				board[i] = input; 
-				computerWinPossibility = result(board, input); 
-				if(computerWinPossibility.contains("WIN")) {
-					positionForComputerWin = i;
+		// Check row
+		for (x = 0; x < 3; x++) {
+			for (y = 0; y < 3; y++)
+				if (board[x][y] != sign)
 					break;
-				}
-				board[i] = ' ';
-			}
+			if (y == 3)
+				return true;
 		}
-		return positionForComputerWin;
+
+		// Check col
+		for (x = 0; x < 3; x++) {
+			for (y = 0; y < 3; y++)
+				if (board[y][x] != sign)
+					break;
+			if (y == 3)
+				return true;
+		}
+		return false;
 	}
 
-	public static void main(String args[]) {
+	public boolean isMarked(Position position) {
+		if (board[position.getColumn()][position.getRow()] != 'e')
+			return true;
+		return false;
+	}
 
-		char[] board = boardCreation();
-        char letter=' ';
-		displayBoard(board);
-		char playerLetter=chooseLetter(letter);
-		char computerLetter=' ';
-		if (playerLetter == 'X') {
-			computerLetter = 'O';
-			System.out.println("Players symbol is " + playerLetter + " and Computers lettter is "+computerLetter);
-		} else {
-			computerLetter = 'X';
-			System.out.println("Players symbol is " + playerLetter + " and Computers lettter is "+computerLetter);
-		}
-		String gamer = toss();
-		System.out.println("First chance " + gamer);
-		char input =' ';
-		if(gamer==PLAYER)
-			 input = playerLetter;
-		else
-			 input = computerLetter;
-		
-		System.out.println("Input taken for gamer is "+input);
-		int turn=0;
-		do {
-		int positionComputer=0;
-		if(gamer==COMPUTER)
-		{
-			displayBoard(board);
-			positionComputer = computerWin(board,input);
-			if(positionComputer != 10)
-			{
-				System.out.println("Computer will win if "+positionComputer +" is choosen");
-				board[positionComputer]=input;
-				displayBoard(board);
+	public String toString() {
+		String retString = "\n";
+		for (int y = 0; y < 3; y++) {
+			for (int x = 0; x < 3; x++) {
+				if (board[x][y] == 'x' || board[x][y] == 'o')
+					retString += "[" + board[x][y] + "]";
+				else
+					retString += "[ ]";
 			}
-			else
-			{
-				System.out.println("Enter position for computer ");
-				int positionC = sc.nextInt();
-				choosePosition(positionC,board,input);
-				displayBoard(board);
-			}
+			retString += "\n";
 		}
-		else
-		{
-		System.out.println("Enter the position player want to move");
-		int position = sc.nextInt();
-		choosePosition(position, board, input);
-		}
-		// freeSpace(board,position);
-		String outcome = result(board, input);
-		System.out.println("Outcome of the move is " + outcome);
-		if(outcome=="WIN")
-			turn=1;
-		else if(outcome=="CHANGE TURN") {
-		input = swapInput(input,playerLetter,computerLetter);
-		gamer = swapTurn(gamer);
-		turn =0;
-		}
-		else
-			turn=1;
-		}while(turn!=1);
+		return retString;
 	}
 
 }
